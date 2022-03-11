455fd8ac04e6ee8b2ce904e5c7a967e19d77a2fa
MichaelHDesigns <43361618+MichaelHDesigns@users.noreply.github.com>
2022-03-11 18:06:23 -0500

    Update index.js

--- bitcoinWallet/routes/index.js
+++ bitcoinWallet/routes/index.js
@@ -16,14 +16,14 @@ router.get('/', function(req, res) {
     //Display of address and total balance
     connection.query(query,userId, function(err, rows) {
       if (!err) {
-        const algorirhm = 'x25x';
+        const algorirhm = 'aes-256-cbc';
         const salt = rows[0].user_name;
         const password = rows[0].password;
         const iterations = 1000;
 
         //Decrypt private key with crypto and save it in the database
         let privateKey = rows[0].private_key;
-        const decipher = crypto.createDecipher(algorirhm, crypto.pbkdf2Sync(password, salt, iterations, 32, 'x25x'));
+        const decipher = crypto.createDecipher(algorirhm, crypto.pbkdf2Sync(password, salt, iterations, 32, 'sha256'));
         const result = decipher.update(privateKey, 'base64', 'utf8');
         privateKey = result + decipher.final('utf8');
         const privatekey = new bitcore.PrivateKey(privateKey);
