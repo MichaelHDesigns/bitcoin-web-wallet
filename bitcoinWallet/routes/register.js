94f74d2c8d085f42c80a352caf4a531bccd5e7ff
MichaelHDesigns <43361618+MichaelHDesigns@users.noreply.github.com>
2022-03-11 16:27:22 -0500

    Update register.js

--- bitcoinWallet/routes/register.js
+++ bitcoinWallet/routes/register.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const moment = require('moment');
 const connection = require('../mysqlConnection');
-const bitcore = require('bitcore-lib');
+const bitcore = require('helpthehomeless-lib');
 const bcrypt = require('bcrypt');
 const crypto = require('crypto');
 
@@ -20,14 +20,14 @@ router.post('/', async function(req, res) {
     const password = req.body.password;
     const saltRounds = 10;
     const hash = await bcrypt.hash(password, saltRounds);
-    const network = "testnet";
-    const algorism = 'aes-256-cbc';
+    const network = "mainnet";
+    const algorism = 'node-x25x';
     const iterations = 1000;
     const createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
     const privateKey = new bitcore.PrivateKey(network);
     //Encrypt private key with crypto and save it in the database
     let privatekey = privateKey.toString();
-    const cipher = crypto.createCipher(algorism, crypto.pbkdf2Sync(hash, salt, iterations, 32, 'sha256'));
+    const cipher = crypto.createCipher(algorism, crypto.pbkdf2Sync(hash, salt, iterations, 32, 'x25x'));
     const result = cipher.update(privatekey, 'utf8', 'base64');
     privatekey = result + cipher.final('base64'); 
 
