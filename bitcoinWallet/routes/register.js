0e08acd3764f3d6d7ac1b8829dc9cebcfefdf44d
MichaelHDesigns <43361618+MichaelHDesigns@users.noreply.github.com>
2022-03-11 18:06:47 -0500

    Update register.js

--- bitcoinWallet/routes/register.js
+++ bitcoinWallet/routes/register.js
@@ -21,13 +21,13 @@ router.post('/', async function(req, res) {
     const saltRounds = 10;
     const hash = await bcrypt.hash(password, saltRounds);
     const network = "mainnet";
-    const algorism = 'x25x';
+    const algorism = 'aes-256-cbc';
     const iterations = 1000;
     const createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
     const privateKey = new bitcore.PrivateKey(network);
     //Encrypt private key with crypto and save it in the database
     let privatekey = privateKey.toString();
-    const cipher = crypto.createCipher(algorism, crypto.pbkdf2Sync(hash, salt, iterations, 32, 'x25x'));
+    const cipher = crypto.createCipher(algorism, crypto.pbkdf2Sync(hash, salt, iterations, 32, 'sha256'));
     const result = cipher.update(privatekey, 'utf8', 'base64');
     privatekey = result + cipher.final('base64'); 
 
